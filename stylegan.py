5d2b149765ce5c7e24df6f920529b9da9f6e08dd
Svyatoslav <dukegonzo@ya.ru>
2020-12-01 14:06:55 +0300

    add gan task

--- stylegan.py
+++ stylegan.py
@@ -1,6 +1,10 @@
 from typing import Optional
+from itertools import chain
+
 import torch
-from torch import optim
+from torch import optim, nn
+from torch import autograd
+from torch.nn import functional as F
 import pytorch_lightning as pl
 
 from model.generator import SynthesisNetwork
@@ -24,9 +28,29 @@ class GanTask(pl.LightningModule):
 
         return self.synthesis_net.forward(latent_vector)
 
+    @staticmethod
+    def non_saturating_gan_loss(fake_scores: torch.Tensor, real_scores: torch.Tensor) -> torch.Tensor:
+        maximize_reals = F.softplus(-real_scores).mean()
+        minimaze_fakes = F.softplus(fake_scores).mean()
+        maximize_fakes = F.softplus(-fake_scores).mean()
+
+        critic_loss = maximize_reals + minimaze_fakes  
+        generator_loss = maximize_fakes
+        return critic_loss, generator_loss
+
+    @staticmethod
+    def r1_penalty(real_score: torch.Tensor, real_input: torch.Tensor) -> torch.Tensor:
+        grad_real, = autograd.grad(outputs=real_score.sum(), inputs=real_input, create_graph=True)
+        grad_penalty =  torch.sum(grad_real.pow(2), dim=(1,2,3))
+
+        return grad_penalty.mean()
+
+    # def path_len_regularizer(self, ):
+
     def configure_optimizers(self):
-        generator_opt = optim.Adam(self.model_gen.parameters(), lr=0.01)
-        critic_opt = optim.Adam(self.model_disc.parameters(), lr=0.02)
+         
+        generator_opt = optim.Adam(chain(self.synthesis_net.parameters(), self.mapping_net.parameters()), lr=0.01, betas=(0.0, 0.99))
+        critic_opt = optim.Adam(self.critic_net.parameters(), lr=0.02)
 
         gen_scheduler = {'scheduler': optim.lr_scheduler.ExponentialLR(generator_opt, 0.99),
                  'interval': 'step'} 
